9eb2a8c016aef73b630b901fb6153020f33036c3
shuhei05 <qoo0228y@gmail.com>
2021-07-13 17:08:37 +0900

    add:view

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+
+    public function showList()
+    {
+        return view('blog.list');
+    }
+}
